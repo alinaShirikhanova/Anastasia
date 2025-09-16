cc4a8c7ab6bd842662523a75da50c99d3c0ac024
Alina <alina.meller.98@mail.ru>
2025-09-16 21:06:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+# печатаем предложение
+print('Hello ', 'world', '!', sep='$')
+# Комментарии
+
+# Распечатать несколько слов, разделив их %
+# print("привет", "мир", sep="%")
+
+# print('Hello\nWorld')
+
+# print("hello\nworld")
+
+# print('Hello', end='&\n')
+# print('World', end='')
+
+# print("spring" , end=" ")
+# print("winter")
+
+# line = 'dfdsjfksdjfkfjsjfkajfksdjfkdjsfksdjffjkaksdjffsjdfk'
+# Переменная - ячейка, в которую можно что-то положить
+# print(line)
+# print(line)
+# print(line)
+# print(line)
+# print(line)
+
+# name = "nastya"
+# print(name)
+
+# a = 'fdkfld'
+# print(a)
+
+# line = 'sksfjksdjf'
+# print(line)
+#
+# line = 'Alina'
+# print(line)
+
+# number1 = '10'
+# number2 = 5
+# print(number1 + number2)
+# print(10 + 5)
+
+# print(number1 + number2)
+# print('10' + '5')
+
+# string - строка
+
+
+# a = '20'
+# g = '20'
+# print(a + g)
+
+# +
+# -
+# *
+# /
+# a = 10
+# b = 3
+# print('a + b =', a + b)
+# print('a - b =', a - b)
+# print('a * b =', a * b)
+# print('a / b =', a / b)
+# print('a // b =', a // b)
+# print('a % b =', a % b)
+
+
+
+
+
+a = 30
+k = 7
+print(a / k)
+print(a // k)
+
+
+
+
+
+
+
