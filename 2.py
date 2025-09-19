ee0088827fa1725245fc00f34aa4cffc4fc91dcc
Alina <alina.meller.98@mail.ru>
2025-09-19 14:15:22 +0300

    2

---
+++ 2.py
@@ -0,0 +1,41 @@
+# login = input()
+# print(login)
+
+# s = input()
+# print(s)
+# print(input())
+
+# print(input())
+
+# a = str(4)
+# b = str(4)
+# print(a + b)
+
+# a = int('10')
+# b = int('10')
+#
+# print(a + b)
+
+# a = str(5)
+# b = int('8')
+
+# int - integer - целое число
+
+# текст = строка - string
+
+# a = int(input())
+# b = int(input())
+# print(a + b)
+
+# a = int(input('Введите первое число: '))
+# g = int(input('Введите второе число: '))
+# d = int(input('Введите третье число: '))
+# print(a * g * d)
+
+
+a = input()
+d = input()
+n = input()
+print(a)
+print(d)
+print(n)
