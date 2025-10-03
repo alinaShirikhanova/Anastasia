aa287ae32d2729891a80cb96365eef689e385c9a
Alina <alina.meller.98@mail.ru>
2025-10-03 22:52:39 +0300

    6

---
+++ 6.py
@@ -0,0 +1,89 @@
+# age = int(input())
+# height = int(input())
+
+# if age >= 18 and height >= 170:
+#     print('да')
+# else:
+#     print('нет')
+
+#
+# if age >= 18:
+#     if height >= 170:
+#         print('Можете проходить')
+#     else:
+#         print('Вы слишком низкий')
+# else:
+#     print('Вы не подходите по возрасту')
+
+
+# hasTicket = True
+# age = int(input())
+#
+# if hasTicket == True:
+#     if age >= 18:
+#         print('Проходите')
+#     else:
+#         print('Неподходящий возраст')
+# else:
+#     print('У вас нет билета')
+
+# Пользователь вводит пол и возраст
+# Девочка
+# Женщина
+# Мальчик
+# Мужчина
+
+# Вложенные условия
+
+
+# a = int(input())
+# b = input()
+#
+# if a >= 18 and b == "ж":
+#     print("Женщина")
+# elif a >= 18 and b == "м":
+#     print("Мужчина")
+# elif a < 18 and b == "ж":
+#     print("Девочка")
+# elif a < 18 and b == "м":
+#     print("Мальчик")
+# else:
+#     print("Некорректный")
+
+
+# Строки - текст
+# l = ("1. Удалить контакт"
+#      "2. ")
+# l = "Настя"
+# l = """1. Удалить контакт
+# """
+# l = '''Настя'''
+
+# '''
+# 1. Удалить контакт
+# 1. Удалить контакт
+# 1. Удалить контакт
+# 1. Удалить контакт
+# 1. Удалить контакт
+# '''
+
+
+# login = 'alina12'
+# print(login[5])
+# print(login[1])
+# print(login[2])
+# print(login[-4])
+# print(login[-7])
+# print(len(login) // 2)
+
+# a = 'y' * 10
+# print(a)
+# a= '123' * 100
+# print('a' + '10')
+a = 'a' * 100 + 'd' * 100
+print(a)
+
+
+
+
+
